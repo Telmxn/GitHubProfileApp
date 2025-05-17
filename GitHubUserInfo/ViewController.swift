33a1eebd929409ff23ba5f0d83a8aa56f0e1b9b2
Telmxn <telmanyusifov78@gmail.com>
2025-05-17 15:54:40 +0400

    Initial Commit.

---
+++ GitHubUserInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubUserInfo
+//
+//  Created by Telman Yusifov on 17.05.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
